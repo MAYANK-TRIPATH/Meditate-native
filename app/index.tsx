244bdf483e063822dce60350ba57b7111fb2a159
MAYANK-TRIPATH <mr.mayanktripathi2001@gmail.com>
2025-01-23 01:23:23 +0530

    Tailwind worked :)

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import "../global.css";
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-gray-600">
+      <Text className="text-white text-lg font-bold">Hello,Bhailog sab badhiya</Text>
+    </View>
+  );
+}
