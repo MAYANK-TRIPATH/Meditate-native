c1ae4e765ae1cd36e82e4bc7b0a205694f3a404e
MAYANK-TRIPATH <mr.mayanktripathi2001@gmail.com>
2025-01-23 17:59:49 +0530

    styling doing

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,43 @@
 import "../global.css";
-import { View, Text } from "react-native";
+import { View, Text, ImageBackground } from 'react-native';
+import beachImage from '@/assets/meditation-images/beach.webp';
+import { LinearGradient } from "expo-linear-gradient";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import CustomButton from "@/components/CustomButton";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-gray-600">
-      <Text className="text-white text-lg font-bold">Hello,Bhailog sab badhiya</Text>
+    <View className="flex-1">
+      <ImageBackground
+        source={beachImage}
+        resizeMode="cover"
+        className="flex-1"
+      >
+        <LinearGradient
+          className="flex-1"
+          colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}
+        >
+          <SafeAreaView className="flex-1 px-1 justify-between">
+            <View>
+              <Text className="text-white text-4xl font-bold text-center">
+                App
+              </Text>
+              <Text className="text-white text-2xl font-semibold text-center">
+                Here you can calm your mind, thoughts and relax.
+              </Text>
+            </View>
+            
+            <View className="pb-4 px-4 bg-white rounded-xl items-center justify-center">
+              <CustomButton
+                onPress={() => console.log("tap")}
+                title="Get Started"
+              />
+            </View>
+            <StatusBar style="light" />
+          </SafeAreaView>
+        </LinearGradient>
+      </ImageBackground>
     </View>
-  );
+  )
 }
